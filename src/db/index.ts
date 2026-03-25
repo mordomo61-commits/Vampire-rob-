02e5b8112aee8e6e226e329993d1f2a59f33369d
mordomo61-commits <mordomo61@gmail.com>
2026-03-25 18:56:51 -0300

    Add files via upload

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,20 +1,19 @@
 import { config } from "dotenv";
-  config();
+config();
 
-  import { drizzle } from "drizzle-orm/node-postgres";
-  import pg from "pg";
-  import * as schema from "./schema";
+import { drizzle } from "drizzle-orm/node-postgres";
+import pg from "pg";
+import * as schema from "./schema";
 
-  const { Pool } = pg;
+const { Pool } = pg;
 
-  if (!process.env.DATABASE_URL) {
-    throw new Error(
-      "DATABASE_URL must be set. Did you forget to set the .env file?",
-    );
-  }
+if (!process.env.DATABASE_URL) {
+  throw new Error(
+    "DATABASE_URL must be set. Defina o arquivo .env com DATABASE_URL."
+  );
+}
 
-  export const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-  export const db = drizzle(pool, { schema });
+export const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+export const db = drizzle(pool, { schema });
 
-  export * from "./schema";
-  
+export * from "./schema";
