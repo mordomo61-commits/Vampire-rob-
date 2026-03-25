7b19c5f2cca8682743ffa6e3b3af78909036fc38
mordomo61-commits <mordomo61@gmail.com>
2026-03-25 16:26:38 -0300

    Add files via upload

---
+++ nome-da-pasta/src/src/eventos/ready.ts
@@ -0,0 +1,19 @@
+import { Events, type Client, ActivityType } from "discord.js";
+
+export const name = Events.ClientReady;
+export const once = true;
+
+export async function execute(client: Client) {
+  console.log(`✅ Bot online como ${client.user?.tag}`);
+  console.log(`📡 Servidores: ${client.guilds.cache.size}`);
+
+  client.user?.setPresence({
+    activities: [
+      {
+        name: `/help | ${client.guilds.cache.size} servidores`,
+        type: ActivityType.Watching,
+      },
+    ],
+    status: "online",
+  });
+}
